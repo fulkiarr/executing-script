141d210077c898e85f6e754ceb04f6b05a5bd964
Fulki arrafi <fulki.arrafi@miota.id>
2019-07-15 16:00:34 +0700

    LICENSE Commit

--- restart.sh
+++ restart.sh
@@ -5,7 +5,17 @@ cd /Users/xshot51/Documents/Bash\ Executing/
 command=`pgrep -f test.py`
 if [ ${#command} -lt 1 ]; then
     echo "Cant find PID on this running program"
+    echo "Do you want to running this program [Y/y or N/n] :"
+    read value
+    case $value in  
+    y|Y) nohup python test.py > /dev/null 2>&1 & ;; 
+    n|N) echo Not running the program ;; 
+    *)  echo Input invalid ;; 
+    esac
+    echo Running the program success 
 else
     `kill -9 $command` 
     echo "Program killed"
+    nohup python test.py > /dev/null 2>&1 &
+    echo "Running the program again"
 fi
